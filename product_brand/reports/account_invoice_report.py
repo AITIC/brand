c3576766be58640cd89d636588d07fe2281a8347
Germana <goliveira@aitic.com.ar>
2023-11-16 16:35:42 -0400

    [FIX] revertir y modificar dependencia

--- product_brand/reports/account_invoice_report.py
+++ product_brand/reports/account_invoice_report.py
@@ -9,9 +9,9 @@ class AccountInvoiceReport(models.Model):
 
     product_brand_id = fields.Many2one(comodel_name="product.brand", string="Brand")
 
-    @property
-    def _table_query(self):
-        return '%s %s %s %s' % (self._select(), self._from(), self._where(), self._group_by())
+    #@property
+    #def _table_query(self):
+    #    return '%s %s %s %s' % (self._select(), self._from(), self._where(), self._group_by())
 
     @api.model
     def _select(self):
@@ -23,37 +23,6 @@ class AccountInvoiceReport(models.Model):
 
     @api.model
     def _group_by(self):
-        return '''
-            GROUP BY
-                line.id,
-                line.move_id,
-                line.product_id,
-                line.account_id,
-                line.analytic_account_id,
-                line.journal_id,
-                line.company_id,
-                line.currency_id,
-                line.partner_id,
-                move.name,
-                move.state,
-                move.move_type,
-                move.amount_residual_signed,
-                move.amount_total_signed,
-                move.partner_id,
-                move.invoice_user_id,
-                move.fiscal_position_id,
-                move.invoice_date,
-                move.date,
-                move.invoice_date_due,
-                move.invoice_payment_term_id,
-                move.payment_state,
-                move.team_id,
-                move.l10n_latam_document_type_id,
-                uom_template.id,
-                uom_line.factor,
-                template.categ_id,
-                COALESCE(partner.country_id, commercial_partner.country_id),
-                currency_table.rate,
-                contact_partner.state_id,
-                template.product_brand_id
-            '''
+        group_by_str = super()._group_by()
+        group_by_str += ", template.product_brand_id"
+        return group_by_str
